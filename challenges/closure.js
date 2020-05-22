8ec7259b67c589d85111a446f64809b5993cd37e
Vince Williams <williamsvm20@gmail.com>
2020-05-22 11:52:25 -0400

    closure and Prototypes are finished

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,20 @@ myFunction();
 
 // Explanation: 
 
+  // internal is accessible by nestedFunction first, because internal is not defined inside nestedFunction; second because nestedFunction is inside myFunction. Since there is no variable named internal inside nestedFunction it looks outside itself an up the page to find the variable. Also internal would be unavailable to nestedFunction if nestedFunction were outside myFunction due to scoping.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+console.log("Task 2:  Counter")
+
+function sumation(num){
+  let sum = 0
+  for (let i=0; i <= num; i++){
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sumation(4));
